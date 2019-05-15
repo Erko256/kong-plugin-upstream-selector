ebf6a61d621dd69e826cd6bbc66e711286946be5
Kapu Erik <erko256@gmail.com>
2019-05-15 16:12:00 +0200

    CDPIP-1378: Set priority and remove boilerplate code

--- kong/plugins/upstream-selector/handler.lua
+++ kong/plugins/upstream-selector/handler.lua
@@ -2,7 +2,7 @@ local BasePlugin = require "kong.plugins.base_plugin"
 
 local UpstreamSelectorHandler = BasePlugin:extend()
 
-UpstreamSelectorHandler.PRIORITY = 2000
+UpstreamSelectorHandler.PRIORITY = 480
 
 function UpstreamSelectorHandler:new()
   UpstreamSelectorHandler.super.new(self, "upstream-selector")
@@ -10,19 +10,6 @@ end
 
 function UpstreamSelectorHandler:access(conf)
   UpstreamSelectorHandler.super.access(self)
-
-  if conf.say_hello then
-    kong.log.debug('Hey!')
-
-    kong.service.request.set_header('X-Upstream-Header', 'Hey Upstream!')
-    kong.response.set_header('X-Downstream-Header', 'Hey Downstream!')
-  else
-    kong.log.debug('Bye!')
-
-    kong.service.request.set_header('X-Upstream-Header', 'Bye Upstream!')
-    kong.response.set_header('X-Downstream-Header', 'Bye Downstream!')
-  end
-
 end
 
 return UpstreamSelectorHandler
