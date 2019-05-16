1b8a6cad8f3dee59ff78121fad404f9618f66196
Kapu Erik <erko256@gmail.com>
2019-05-16 15:17:28 +0200

    CDPIP-1378: Respond with error if upstream does not exist
    Co-Authored-By: gaborb <gabor.barnak@gmail.com>

--- kong/plugins/upstream-selector/handler.lua
+++ kong/plugins/upstream-selector/handler.lua
@@ -1,15 +1,32 @@
+local responses = require "kong.tools.responses"
 local BasePlugin = require "kong.plugins.base_plugin"
+local Logger = require "logger"
 
 local UpstreamSelectorHandler = BasePlugin:extend()
 
 UpstreamSelectorHandler.PRIORITY = 480
 
 function UpstreamSelectorHandler:new()
-  UpstreamSelectorHandler.super.new(self, "upstream-selector")
+    UpstreamSelectorHandler.super.new(self, "upstream-selector")
 end
 
 function UpstreamSelectorHandler:access(conf)
-  UpstreamSelectorHandler.super.access(self)
+    UpstreamSelectorHandler.super.access(self)
+
+    local headers = ngx.req.get_headers()
+    local header_value = headers[conf.header_name]
+    if not header_value then
+        return
+    end
+
+    local ok, _ = kong.service.set_upstream(header_value)
+    if not ok then
+        Logger.getInstance(ngx):logInfo({
+            msg = "Upstream does not exist",
+            upstream = header_value
+        })
+        responses.send(400, "Bad request")
+    end
 end
 
 return UpstreamSelectorHandler
--- spec/upstream-selector/plugin_e2e_spec.lua
+++ spec/upstream-selector/plugin_e2e_spec.lua
@@ -20,46 +20,93 @@ describe("UpstreamSelector", function()
         helpers.db:truncate()
     end)
 
-    context("Admin API", function()
-        local service
+    describe("Admin API", function()
+        local consumer
+
+        before_each(function()
+            consumer = kong_sdk.consumers:create({
+                username = "test-consumer"
+            })
+        end)
+
+        context("Plugin configuration", function()
+
+            context("when header name is present", function()
+                it("should create the plugin", function()
+                    local success, _ = pcall(function()
+                        kong_sdk.plugins:create({
+                            consumer_id = consumer.id,
+                            name = "upstream-selector",
+                            config = {
+                                header_name = "Test-Header"
+                            }
+                        })
+                    end)
+
+                    assert.is_true(success)
+                end)
+            end)
+
+            context("when header name is missing", function()
+                it("should respond with an error", function()
+                    local success, _ = pcall(function()
+                        kong_sdk.plugins:create({
+                            consumer_id = consumer.id,
+                            name = "upstream-selector",
+                            config = {}
+                        })
+                    end)
+
+                    assert.is_false(success)
+                end)
+            end)
+
+        end)
+    end)
+
+    describe("Upstream selector", function()
+        local service, route, plugin
 
         before_each(function()
             service = kong_sdk.services:create({
                 name = "test-service",
                 url = "http://mockbin:8080/request"
             })
+            route = kong_sdk.routes:create_for_service(service.id, "/test-route")
+            plugin = kong_sdk.services:add_plugin(service.id, {
+                name = "upstream-selector",
+                config = {
+                    header_name = "Test-Header"
+                }
+            })
         end)
 
-        context('Plugin configuration', function()
+        context("when upstream is missing", function()
+            it("should respond with error", function()
+                local response = send_request({
+                    method = "GET",
+                    path = "/test-route",
+                    headers = {
+                        ["Test-Header"] = "NonExistingUpstream"
+                    }
+                })
 
-            it('should return 201 when header name is given', function()
-                local success, response = pcall(function()
-                    kong_sdk.plugins:create({
-                        service_id = service.id,
-                        name = "upstream-selector",
-                        config = {
-                            header_name = "header_name"
-                        }
-                    })
-                end)
-
-                assert.is_true(success)
+                assert.are.equal(400, response.status)
             end)
+        end)
 
-            it('should respond with error when header name is missing', function()
-                local success, response = pcall(function()
-                    kong_sdk.plugins:create({
-                        service_id = service.id,
-                        name = "upstream-selector",
-                        config = {}
-                    })
-                end)
+        context("when header is missing", function()
+            it("should do nothing", function()
+                local response = send_request({
+                    method = "GET",
+                    path = "/test-route"
+                })
 
-                assert.is_false(success)
+                assert.are.equal(200, response.status)
             end)
-
         end)
 
     end)
+
 
 end)
